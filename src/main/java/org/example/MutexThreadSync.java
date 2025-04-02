707d8f198ab8b2be2c04b1aaaa2d896af992c553
Ranveer225 <singr12@farmingdale.edu>
2025-04-01 22:12:28 -0400

    implement data class with read and write

---
+++ src/main/java/org/example/MutexThreadSync.java
@@ -0,0 +1,30 @@
+package org.example;
+
+class MutexData {
+    int data;
+
+    synchronized void write() {
+        data = 42;
+        System.out.println("Write " + data);
+    }
+
+    synchronized void read() {
+        System.out.println("Read " + data);
+    }
+}
+
+public class MutexThreadSync {
+    public static void main(String[] args) throws InterruptedException {
+        MutexData shared = new MutexData();
+
+        Thread writer = new Thread(shared::write);
+
+        Thread reader = new Thread(shared::read);
+
+        writer.start();
+        writer.join();
+
+        reader.start();
+        reader.join();
+    }
+}
